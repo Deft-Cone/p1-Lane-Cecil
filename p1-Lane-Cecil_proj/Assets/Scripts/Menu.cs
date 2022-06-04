c1ad09d962bb8113203beda331ac9c19262fb28d
Cecil Kong <clkong@usc.edu>
2022-06-04 12:15:46 -0500

    Buttons, Collectables, etc.

---
+++ p1-Lane-Cecil_proj/Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void ReturnToMenu ()
+    {
+        SceneManager.LoadScene("Start Scene");
+    }
+    
+    public void TurnVFXOn ()
+    {
+        
+    }
+    public void Pause ()
+    {
+        
+    }
+
+    public void Volume ()
+    {
+        
+    }
+}
