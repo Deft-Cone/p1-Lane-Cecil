93043a34f183f0931328df1af39e0704597bd795
Cecil Kong <clkong@usc.edu>
2022-06-04 20:57:41 -0500

    Revert "Merge branch 'Collectables' into CollectablesRe"
    This reverts commit 57d13afa78363cd087ca3ce261ea310f1324fb8b, reversing
    changes made to fbfabdb9a41155195474610968f9fd58bd9ec72f.

--- p1-Lane-Cecil_proj/Assets/Scripts/Menu.cs
+++ p1-Lane-Cecil_proj/Assets/Scripts/Menu.cs
@@ -5,8 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    public GameObject pauseMenuScreen;
-    public bool VFX = false; 
     public void ReturnToMenu ()
     {
         SceneManager.LoadScene("Start Scene");
@@ -14,24 +12,15 @@ public class Menu : MonoBehaviour
     
     public void TurnVFXOn ()
     {
-        if (VFX)
-        {
-            VFX = false;
-        }
-        else
-        {
-            VFX = true; 
-        }
+        
     }
     public void Pause ()
     {
-        pauseMenuScreen.SetActive(true);
-        Time.timeScale = 0f;
+        
     }
-    
-    public void Resume ()
+
+    public void Volume ()
     {
-        pauseMenuScreen.SetActive(false);
-        Time.timeScale = 1f;
+        
     }
 }
